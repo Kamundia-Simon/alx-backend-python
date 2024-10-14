94fbcbaacd492cc2436123923ea6c02838b54960
Kamundia-Simon <simonkamundia8@gmail.com>
2024-10-14 17:00:12 +0300

    type annotated function for sum

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+function for two floats  and returns sum
+"""
+
+def add(a: float, b: float) -> float:
+    """ Type annotated function with 2 floats and returns sum
+
+    Args:
+    a (float): float number
+    b (float): second float number
+
+    Returns:
+    sum: float
+    """
+
+    return a + b
