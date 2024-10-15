6e1bc4146e06dc37a1f16315a03611ad3abe676b
Kamundia-Simon <simonkamundia8@gmail.com>
2024-10-15 21:28:40 +0300

    fixed pep

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-function that akes a list input_list of 
+function that akes a list input_list of
 floats as argument and returns their sum as a float.
 """
 from typing import List
