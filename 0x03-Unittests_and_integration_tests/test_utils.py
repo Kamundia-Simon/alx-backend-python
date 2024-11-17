71d86f112cab7fa87696170de72c64811126a93a
Kamundia-Simon <simonkamundia8@gmail.com>
2024-11-17 19:57:46 +0300

    parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ Unit tests for utils.access_nested_map
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Dict, Tuple, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -18,3 +19,18 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected) -> None:
         """Test that access_nested_map output"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError, "Key 'a' not found"),
+        ({"a": 1}, ("a", "b"), KeyError, "Key 'b' not found under 'a'")
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            expected_message: str,
+            ) -> None:
+        """Tests `access_nested_map` raises correct message."""
+        with self.assertRaises(Exception) as context:
+            access_nested_map(nested_map, path)
